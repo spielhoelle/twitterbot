194554f166c06a048adbeaf45b4b8a2c8c2e9ec5
Thomas Kuhnert <spiel.hoelle@live.de>
2018-01-18 20:17:05 +0100

    add botconfig and dotenv

---
+++ config.js
@@ -0,0 +1,12 @@
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+  consumer_key: CONSUMER_KEY,  
+  consumer_secret: CONSUMER_SECRET,
+  access_token: ACCESS_TOKEN,  
+  access_token_secret: ACCESS_TOKEN_SECRET
+}
