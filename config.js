3b40383d2c6920909f83a09ee9a6930a35398f26
Thomas Kuhnert <spiel.hoelle@live.de>
2018-01-18 20:28:29 +0100

    run bot

--- config.js
+++ config.js
@@ -4,9 +4,11 @@
  * access_token
  * access_token_secret
  */
+require('dotenv').config()
+
 module.exports = {
-  consumer_key: CONSUMER_KEY,  
-  consumer_secret: CONSUMER_SECRET,
-  access_token: ACCESS_TOKEN,  
-  access_token_secret: ACCESS_TOKEN_SECRET
+  consumer_key: process.env.CONSUMER_KEY,  
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,  
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
 }
